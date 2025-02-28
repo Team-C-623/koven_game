096193cf65642b6d18680c7abde19ea3e3d24e1e
Vidur Pandiripally <vkpandiripally@gmail.com>
2025-02-28 16:31:45 -0600

    Character now switches sides when going left, plus ajusted camera angle

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody3D
 
-
+@onready var sprite_3d: Sprite3D = $Sprite3D
 const SPEED = 6.0
 
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
+	if Input.is_action_just_pressed("right"):
+		# when player hits "right", shows original orientation
+		sprite_3d.scale.x = 1
+	elif Input.is_action_just_pressed("left"):
+		#when player hits "left", mirros character
+		sprite_3d.scale.x = -1
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
