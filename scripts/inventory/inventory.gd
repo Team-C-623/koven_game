7517a37a7fda01c383af3a7759dbe24b4c0fe30b
hvwendt <hvwendt@gmail.com>
2025-04-24 15:37:19 -0500

    alskdjf

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -5,6 +5,7 @@ const Slot = preload("res://scenes/inventory/slot.tscn")
 @onready var background_frame: TextureRect = $BackgroundFrame
 @onready var margin_container: MarginContainer = $BackgroundFrame/MarginContainer
 @onready var item_grid: GridContainer = $BackgroundFrame/MarginContainer/ItemGrid
+@onready var inventory: PanelContainer = $"."
 
 
 
@@ -14,13 +15,14 @@ func _ready():
 	background_frame.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
 	background_frame.stretch_mode = TextureRect.STRETCH_SCALE
 	
-	margin_container.add_theme_constant_override("margin_left", 10)
-	margin_container.add_theme_constant_override("margin_right", 10)
-	margin_container.add_theme_constant_override("margin_top", 10)
-	margin_container.add_theme_constant_override("margin_bottom", 10)
-	
-	background_frame.z_index = 5  # Draw behind slots
-	#item_grid.z_index = 1  # Draw above frame
+	#margin_container.add_theme_constant_override("margin_left", 10)
+	#margin_container.add_theme_constant_override("margin_right", 10)
+	#margin_container.add_theme_constant_override("margin_top", 10)
+	#margin_container.add_theme_constant_override("margin_bottom", 10)
+	#margin_container.modulate.a = 0.5
+	#inventory.modulate.a = 0.5
+	background_frame.z_index = 2  # Draw behind slots
+	item_grid.z_index = 1 # Draw above frame
 	
 func set_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_updated.connect(populate_item_grid)
