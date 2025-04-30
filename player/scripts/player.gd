200feb5aba90f24e672a3f4d150dbecb4c1e6480
hvwendt <hvwendt@gmail.com>
2025-04-30 18:54:10 -0500

    stop wwise errors a little

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -69,9 +69,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		interact()
 
 func _physics_process(_delta: float) -> void:
-	rtpc.set_value(rtpc_node,health_component.health)
-	rtpc_node.post_event()
-	print(rtpc)
+	#rtpc.set_value(rtpc_node,health_component.health)
+	#rtpc_node.post_event()
+	#print(rtpc)
 	if !is_on_floor():
 		velocity.y -= gravity * _delta
 	# Get the input direction and handle the movement/deceleration.
