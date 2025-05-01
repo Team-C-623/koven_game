abe218d52e5a5f2402c6ca2f414f2cb3411b5c42
jwund <jwunderlich@ku.edu>
2025-04-30 19:42:42 -0500

    bullet fix

--- weapons/flame.gd
+++ weapons/flame.gd
@@ -1,6 +1,6 @@
 extends Node3D
 class_name Flame
-const SPEED = 40.0
+const SPEED = 30.0
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
@@ -8,20 +8,28 @@ const SPEED = 40.0
 
 @export var attack_damage:= 10.0
 
+func _ready():
+	var death_timer = Timer.new()
+	death_timer.wait_time = 3.0
+	death_timer.timeout.connect(_die)
+	death_timer.autostart = true
+	add_child(death_timer)
+
 func _physics_process(delta: float) -> void:
 	position += transform.basis * Vector3(0,0, -SPEED) * delta
-	
 	ray.force_raycast_update()
-	if ray.is_colliding():
+	if ray.is_colliding() and (ray.get_collider() is StaticBody3D or ray.get_collider() is CSGPolygon3D):
+		print("Hitting wall")
 		_die()
-		return
-		
+
 func _die() -> void:
 	mesh.visible = false
 	particles.emitting = true
+	print("PARTICLES!")
 	await get_tree().create_timer(1.0).timeout
+	print("DEATH!")
 	queue_free()
-	
+
 func _on_flame_hit_box_area_entered(area):
 	if area is HitboxComponent:
 		print("Hitting enemy")
