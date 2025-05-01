7ad75a605c0fbc6764dd3cefbaa967bdfa9fd85e
Anakha Krishna <apkrishna16@gmail.com>
2025-04-30 20:22:56 -0500

    add scene transitions
    global scene transition manager
    scene transition for trial room and leaving catacombs

--- catacombs/scripts/dialogue_area.gd
+++ catacombs/scripts/dialogue_area.gd
@@ -5,4 +5,6 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		TarotManager.judgement_active = false
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file("res://trialroom/trialroom1.tscn")
--- catacombs/scripts/entrance.gd
+++ catacombs/scripts/entrance.gd
@@ -5,4 +5,6 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		SoundManager.play_music()
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		main.entered.emit()
