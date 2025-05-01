59ddbdfc01e513026c8bdc07b2ae604da2676621
Anakha Krishna <apkrishna16@gmail.com>
2025-05-01 01:52:43 -0500

    start screen wiring, intro scenes and spawn in cata
    also changed to exclusive fullscreen

--- cutscenes/scripts/spawn_message.gd
+++ cutscenes/scripts/spawn_message.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 func _ready():
 	var balloon_scene = preload("res://cutscenes/scenes/quote_balloon.tscn")
 	DialogueManager.dialogue_ended.connect(_on_dialogue_finished)
-	DialogueManager.show_dialogue_balloon_scene(balloon_scene, load("res://cutscenes/dialogue/intro_quote.dialogue"), "start")
+	DialogueManager.show_dialogue_balloon_scene(balloon_scene, load("res://cutscenes/dialogue/spawn_message.dialogue"), "start")
 
 func _on_dialogue_finished(result):
 	TransitionScreen.transition()
 	await TransitionScreen.on_transition_finished
-	get_tree().change_scene_to_file("res://cutscenes/scenes/spawn_message.tscn")
+	get_tree().change_scene_to_file("res://main/scenes/main.tscn")
