3327528a10d8a254df7008079f7bf27724930a30
Anakha Krishna <apkrishna16@gmail.com>
2025-05-01 02:44:41 -0500

    center text for cutscenes

--- cutscenes/scripts/quote.gd
+++ cutscenes/scripts/quote.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _ready():
-	var balloon_scene = preload("res://cutscenes/scenes/quote_balloon.tscn")
+	var balloon_scene = preload("res://cutscenes/scenes/quote_balloon.tscn").instantiate()
 	DialogueManager.dialogue_ended.connect(_on_dialogue_finished)
 	DialogueManager.show_dialogue_balloon_scene(balloon_scene, load("res://cutscenes/dialogue/intro_quote.dialogue"), "start")
 
