d1242b11f5a790a03dac746e6a4a04476edbea21
jwund <jwunderlich@ku.edu>
2025-05-01 05:55:06 -0500

    shop, particles, bugs
    added shop assets
    updated wand particles and bullet
    fixed some bugs with the shop
    fixed bugs with shooting

--- weapons/flame.gd
+++ weapons/flame.gd
@@ -4,13 +4,15 @@ const SPEED = 30.0
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
-@onready var particles = $GPUParticles3D
+@onready var sparks_particles: GPUParticles3D = $SparksParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var light: OmniLight3D = $OmniLight3D
 
 @export var attack_damage:= 10.0
 
 func _ready():
 	var death_timer = Timer.new()
-	death_timer.wait_time = 3.0
+	death_timer.wait_time = 2.0
 	death_timer.timeout.connect(_die)
 	death_timer.autostart = true
 	add_child(death_timer)
@@ -20,21 +22,23 @@ func _physics_process(delta: float) -> void:
 	if ray != null:
 		ray.force_raycast_update()
 		if ray.is_colliding() and (ray.get_collider() is StaticBody3D or ray.get_collider() is CSGPolygon3D):
-			print("Hitting wall")
 			_die()
 
 func _die() -> void:
-	ray.queue_free()
+	if ray != null:
+		ray.queue_free()
 	mesh.visible = false
-	particles.emitting = true
+	light.visible = false
+	explosion_particles.emitting = true
+	sparks_particles.emitting = true
 	await get_tree().create_timer(1.0).timeout
 	queue_free()
 
 func _on_flame_hit_box_area_entered(area):
 	if area is HitboxComponent:
-		print("Hitting enemy")
 		var hitbox : HitboxComponent = area
 		var attack = Attack.new()
 		attack.attack_damage = attack_damage
 		hitbox.damage(attack)
-		_die()
+		if ray != null:
+			_die()
