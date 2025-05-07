3d71e9985294374ae6c7cd2dc8789ba1ea903ddb
hvwendt <hvwendt@gmail.com>
2025-05-07 18:08:01 -0500

    trial room music fix

--- sound/sound_manager.gd
+++ sound/sound_manager.gd
@@ -12,8 +12,10 @@ func _ready() -> void:
 	Wwise.set_state("AMBIENCE_FLOOR","FLOOR1")
 	
 func _process(_delta: float) -> void:
-	if PlayerManager.player != null:
+	if PlayerManager.is_in_trial_room == false:
 		Wwise.set_rtpc_value("Health", PlayerManager.player.health_component.health,self)
+	else:
+		Wwise.set_rtpc_value("Health", 100,self)
 
 func play_footsteps():
 	Wwise.post_event_id(AK.EVENTS.FOOTSTEPS_01, self)
